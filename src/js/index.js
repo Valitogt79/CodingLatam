34cfd16b45e9a9b32d11e695ff6119a988429a14
Oswaldo Alonzo <oalonzo@gmail.com>
2024-10-09 09:31:23 -0600

    Chore: The index.js file has been rewritten, but the error persists when displaying four images in services.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 async function getServices() {
-  const response = await fetch("http://localhost:3000/services");
-  const services = await response.json();
-  console.log(services);
+  const response = await fetch("http://localhost:3000/services")
+  const services = await response.json()
 
   services.map((service) =>
     createServiceItem(
@@ -9,36 +8,36 @@ async function getServices() {
       service.title,
       service.description,
       "Learn More",
-      service.link,
-    ),
-  );
+      service.link
+    )
+  )
 }
 
-getServices();
+getServices()
 
-function createServiceItem(imageUrl, title, description, linkText, linkHref) {
-  const servicesList = document.querySelector(".services-list");
-  const serviceItemContainer = document.createElement("div");
-  serviceItemContainer.classList.add("services-item");
+function createServiceItem(imageURL, title, description, linkText, linkHref) {
+  const servicesList = document.querySelector(".services-list")
+  const serviceItemContainer = document.createElement("div")
+  serviceItemContainer.classList.add("services-item")
 
-  const imageElement = document.createElement("img");
-  imageElement.setAttribute("src", imageUrl);
-  serviceItemContainer.append(imageElement);
+  const imageElement = document.createElement("img")
+  imageElement.setAttribute("src", imageURL)
+  serviceItemContainer.append(imageElement)
 
-  const titleElement = document.createElement("h3");
-  titleElement.textContent = title;
-  serviceItemContainer.append(titleElement);
+  const titleElement = document.createElement("h3")
+  titleElement.textContent = title
+  serviceItemContainer.append(titleElement)
 
-  const paragraph = document.createElement("p");
-  paragraph.textContent = description;
-  serviceItemContainer.append(paragraph);
+  const paragraph = document.createElement("p")
+  paragraph.textContent = description
+  serviceItemContainer.append(paragraph)
 
-  const link = document.createElement("a");
-  link.textContent = linkText;
-  link.setAttribute("href", linkHref);
-  serviceItemContainer.append(link);
+  const link = document.createElement("a")
+  link.textContent = linkText
+  link.setAttribute("href", linkHref)
+  serviceItemContainer.append(link)
 
-  servicesList.append(serviceItemContainer);
+  servicesList.append(serviceItemContainer)
 }
 
-createServiceItem();
+createServiceItem()
